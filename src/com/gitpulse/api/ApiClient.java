bb9f44eac1793998b5d8cafb4cf1fbc4c0ccb24f
Azhan Ali <fe4rtoxin45@gmail.com>
2026-04-11 01:42:34 +0500

    Add abstract ApiClient base class

---
+++ src/com/gitpulse/api/ApiClient.java
@@ -0,0 +1,23 @@
+package com.gitpulse.api;
+
+public abstract class ApiClient {
+    // Declaring Attributes
+    protected final String token;
+    protected final String baseUrl;
+
+    // Constructor
+    public ApiClient(String token, String baseUrl) {
+        this.token = token;
+        this.baseUrl = baseUrl;
+    }
+
+    // Abstract methods
+    public abstract String fetchRepositoryInfo(String owner, String repo) throws Exception;
+    public abstract String fetchCommits(String owner, String repo) throws Exception;
+    public abstract String fetchContributors(String owner, String repo) throws Exception;
+
+    // Concrete method to check if request is successful
+    protected boolean isValidResponse(int statusCode) {
+        return statusCode == 200;
+    }
+}
