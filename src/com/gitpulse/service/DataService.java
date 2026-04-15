5f0415f5a17b4e6aa4d1481682d50c25f9f5d8dc
Azhan Ali <fe4rtoxin45@gmail.com>
2026-04-16 00:18:39 +0500

    Remove Comments from DataService Class

--- src/com/gitpulse/service/DataService.java
+++ src/com/gitpulse/service/DataService.java
@@ -6,39 +6,6 @@ import com.gitpulse.model.Repository;
 import com.gitpulse.parser.*;
 import com.gitpulse.util.ErrorHandler;
 
-//public class DataService {
-//    // Declaring attributes
-//    private final GitHubApiClient apiClient;
-//    private final GitPulseJsonParser parser;
-//
-//    // Constructor
-//    public DataService() {
-//        this.apiClient = new GitHubApiClient();
-//        this.parser    = new GitPulseJsonParser();
-//    }
-//
-//    // Method to return all the repository data
-//    public Repository loadRepository(String owner, String repositoryName) {
-//        Repository repository = new Repository(owner, repositoryName);
-//
-//        try {
-//            // Fetch and parse repository info
-//            String repoJson = apiClient.fetchRepositoryInfo(owner, repositoryName);
-//            parser.parseRepositoryInfo(repoJson, repository);
-//
-//            // Fetch and parse commits
-//            String commitsJson = apiClient.fetchCommits(owner, repositoryName);
-//            parser.parseCommits(commitsJson, repository);
-//
-//            // Fetch and parse contributors
-//            String contributorsJson = apiClient.fetchContributors(owner, repositoryName);
-//            parser.parseContributors(contributorsJson, repository);
-//        } catch (Exception e) {
-//            System.err.println("Error loading repository: " + e.getMessage());
-//        }
-//        return repository;
-//    }
-//}
 public class DataService {
     // Declaring attributes
     private final GitHubApiClient apiClient;
